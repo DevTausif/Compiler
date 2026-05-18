78dd15a3974ef6eecc86500084a47574d3f0336d
DevTausif <kabirtausif610@gmail.com>
2026-05-19 00:14:52 +0600

    changes

--- MID/mid/main.cpp
+++ MID/mid/main.cpp
@@ -1,81 +1,99 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
 
 using namespace std;
 
-// Manual string compare
-int compareString(char a[], char b[])
+
+
+int checkLetter(char ch)
+{
+    if(ch >= 'a' && ch <= 'z')
+        return 1;
+
+    if(ch >= 'A' && ch <= 'Z')
+        return 1;
+
+    return 0;
+}
+
+
+
+int checkDigit(char ch)
+{
+    if(ch >= '0' && ch <= '9')
+        return 1;
+
+    return 0;
+}
+
+
+
+int sameString(char a[], char b[])
 {
     int i = 0;
 
-    while (a[i] != '\0' && b[i] != '\0')
+    while(a[i] != '\0' && b[i] != '\0')
     {
-        if (a[i] != b[i])
+        if(a[i] != b[i])
             return 0;
 
         i++;
     }
 
-    if (a[i] == '\0' && b[i] == '\0')
+    if(a[i] == '\0' && b[i] == '\0')
         return 1;
 
     return 0;
 }
 
-// Check letter
-int isLetter(char ch)
+
+
+int checkKeyword(char word[])
 {
-    if ((ch >= 'a' && ch <= 'z') ||
-        (ch >= 'A' && ch <= 'Z'))
+    char kw1[] = "int";
+    char kw2[] = "float";
+    char kw3[] = "char";
+    char kw4[] = "if";
+    char kw5[] = "else";
+    char kw6[] = "return";
+
+    if(sameString(word, kw1))
+        return 1;
+
+    if(sameString(word, kw2))
+        return 1;
+
+    if(sameString(word, kw3))
+        return 1;
+
+    if(sameString(word, kw4))
+        return 1;
+
+    if(sameString(word, kw5))
+        return 1;
+
+    if(sameString(word, kw6))
         return 1;
 
     return 0;
 }
 
-// Check digit
-int isDigit(char ch)
-{
-    if (ch >= '0' && ch <= '9')
-        return 1;
 
-    return 0;
-}
 
-// Keyword check
-int isKeyword(char str[])
-{
-    char k1[] = "int";
-    char k2[] = "float";
-    char k3[] = "char";
-    char k4[] = "if";
-    char k5[] = "else";
-    char k6[] = "return";
-
-    if (compareString(str, k1)) return 1;
-    if (compareString(str, k2)) return 1;
-    if (compareString(str, k3)) return 1;
-    if (compareString(str, k4)) return 1;
-    if (compareString(str, k5)) return 1;
-    if (compareString(str, k6)) return 1;
-
-    return 0;
-}
-
-// Identifier check
-int isIdentifier(char str[])
+int checkIdentifier(char word[])
 {
     int i = 0;
 
-    if (!(isLetter(str[0]) || str[0] == '_'))
+    if(!(checkLetter(word[0]) || word[0] == '_'))
         return 0;
 
     i = 1;
 
-    while (str[i] != '\0')
+    while(word[i] != '\0')
     {
-        if (!(isLetter(str[i]) ||
-              isDigit(str[i]) ||
-              str[i] == '_'))
+        if(!(checkLetter(word[i]) ||
+             checkDigit(word[i]) ||
+             word[i] == '_'))
         {
             return 0;
         }
@@ -86,95 +104,108 @@ int isIdentifier(char str[])
     return 1;
 }
 
-// Print token type
-void identifyToken(char token[])
+
+
+void showToken(char word[])
 {
-    if (isKeyword(token))
+    if(checkKeyword(word))
     {
-        cout << token << " -> Keyword" << endl;
+        cout << word << " is Keyword" << endl;
     }
-    else if (isIdentifier(token))
+
+    else if(checkIdentifier(word))
     {
-        cout << token << " -> Identifier" << endl;
+        cout << word << " is Identifier" << endl;
     }
+
     else
     {
-        cout << token << " -> Invalid Token" << endl;
+        cout << word << " is Invalid" << endl;
     }
 }
 
-// Print file content
-void printFile()
+
+
+
+void displayFile()
 {
-    ifstream file("E:\\Compiler\\MID\\mid\\myfile.txt");
+    ifstream inputFile("E:\\Compiler\\MID\\mid\\myfile.txt");
 
     char ch;
 
-    cout << "File Content:\n";
+    cout << "File Content :" << endl << endl;
 
-    while (file >> noskipws >> ch)
+    while(inputFile >> noskipws >> ch)
     {
         cout << ch;
     }
 
-    file.close();
+    inputFile.close();
 }
 
-// Process tokens
-void processFile()
+
+
+
+void tokenProcess()
 {
-    ifstream file("E:\\Compiler\\MID\\mid\\myfile.txt");
+    ifstream inputFile("E:\\Compiler\\MID\\mid\\myfile.txt");
 
     char ch;
-    char token[100];
+    char word[100];
 
-    int index = 0;
+    int pos = 0;
 
-    cout << "\n\nToken Identification:\n";
+    cout << endl;
+    cout << endl;
+    cout << "Detected Tokens :" << endl << endl;
 
-    while (file >> noskipws >> ch)
+    while(inputFile >> noskipws >> ch)
     {
-        if (ch == ' ' ||
-            ch == '\n' ||
-            ch == ';' ||
-            ch == '(' ||
-            ch == ')' ||
-            ch == '{' ||
-            ch == '}' ||
-            ch == ',')
+        if(ch == ' '  ||
+           ch == '\n' ||
+           ch == ';'  ||
+           ch == '('  ||
+           ch == ')'  ||
+           ch == '{'  ||
+           ch == '}'  ||
+           ch == ',')
         {
-            if (index > 0)
+            if(pos > 0)
             {
-                token[index] = '\0';
+                word[pos] = '\0';
 
-                identifyToken(token);
+                showToken(word);
 
-                index = 0;
+                pos = 0;
             }
         }
+
         else
         {
-            token[index] = ch;
-            index++;
+            word[pos] = ch;
+
+            pos++;
         }
     }
 
-    if (index > 0)
-    {
-        token[index] = '\0';
 
-        identifyToken(token);
+    if(pos > 0)
+    {
+        word[pos] = '\0';
+
+        showToken(word);
     }
 
-    file.close();
+    inputFile.close();
 }
+
+
 
 int main()
 {
-    printFile();
+    displayFile();
 
-    processFile();
+    tokenProcess();
 
     return 0;
 }
-
